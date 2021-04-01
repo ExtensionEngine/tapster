8aa3d707b2b5f896a09ef97371f1c57f6f4d0d3c
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-04-01 09:38:27 +0200

    Initial commit 🎉

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: 'semistandard',
+  rules: {
+    'space-before-function-paren': 'off'
+  },
+  env: {
+    mocha: true
+  }
+};
