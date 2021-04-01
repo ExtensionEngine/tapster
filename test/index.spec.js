d6f015b501119536e07554d106638bbfb8ddffeb
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-04-01 09:41:25 +0200

    Implement cache manager and providers ✨
    Implement in-memory provider (LRU) and redis provider

---
+++ test/index.spec.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const Memory = require('../lib/providers/memory');
+
+describe('CacheManager', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('constructor', () => {
+    it('should use memory provider when the store is set to "memory"', () => {
+      const store = 'memory';
+      const CacheManager = require('../lib');
+      const cache = new CacheManager({ store });
+      expect(cache.provider.name).to.be.eq(store);
+    });
+
+    it('should use memory provider by default', () => {
+      const store = 'memory';
+      const CacheManager = require('../lib');
+      const cache = new CacheManager();
+      expect(cache.provider.name).to.be.eq(store);
+    });
+
+    it('should use redis provider when the store is set to "redis"', () => {
+      const store = 'redis';
+      class Redis {
+        constructor() {
+          this.name = store;
+        }
+
+        static create() {
+          return new this();
+        }
+      }
+      const CacheManager = proxyquire('../lib', {
+        './providers/redis': Redis
+      });
+      const cache = new CacheManager({ store });
+      expect(cache.provider.name).to.be.eq(store);
+    });
+
+    it('should use custom store provider when the store is set to custom provider instance', () => {
+      const store = 'custom-store';
+      class CustomStore {
+        constructor() {
+          this.name = store;
+        }
+
+        static create() {
+          return new this();
+        }
+      }
+      const CacheManager = require('../lib');
+      const cache = new CacheManager({ store: CustomStore });
+      expect(cache.provider.name).to.be.eq(store);
+    });
+  });
+
+  describe('should call provider methods', () => {
+    const methods = ['set', 'get', 'has', 'getKeys', 'delete'];
+    methods.forEach(method => {
+      it(`should call provider ${method} method`, () => {
+        const stubMethod = sinon.stub(Memory.prototype, method).callsFake(() => {});
+        const CacheManager = proxyquire('../lib', {
+          './providers/memory': Memory
+        });
+        const cache = new CacheManager();
+        cache[method]();
+        expect(stubMethod.calledOnce).to.be.true;
+      });
+    });
+  });
+});
---
+++ test/providers/redis.spec.js
@@ -0,0 +1,86 @@
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+
+describe('Redis provider', () => {
+  const options = { host: 'localhost', port: 6379 };
+
+  afterEach(() => sinon.restore());
+
+  describe('create method', () => {
+    it('should have a create property', () => {
+      const Redis = require('../../lib/providers/redis');
+      expect(Redis).to.have.a.property('create');
+    });
+
+    it('create should be a function', () => {
+      const Redis = require('../../lib/providers/redis');
+      expect(Redis.create).to.be.a('function');
+    });
+
+    it('create should return the class instance', () => {
+      class IoRedis {}
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      const client = Redis.create(options);
+      expect(client).to.be.an.instanceOf(Redis);
+    });
+
+    it('create should throw an error when the host is not provided', () => {
+      class IoRedis {}
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      expect(() => Redis.create({ port: 6379 })).to.throw;
+    });
+
+    it('create should throw an error when the port is not provided', () => {
+      class IoRedis {}
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      expect(() => Redis.create({ host: 'localhost' })).to.throw;
+    });
+  });
+
+  describe('set, get, has, getKeys, delete methods', () => {
+    const Redis = require('../../lib/providers/redis');
+    const methods = ['set', 'get', 'has', 'getKeys', 'delete'];
+    methods.forEach(method => {
+      it(`should have ${method} method`, () => {
+        expect(Redis.prototype).to.have.a.property(method);
+      });
+    });
+  });
+
+  describe('set()', () => {
+    it('set method should not be called with EX parameter when ttl is 0', () => {
+      class IoRedis {
+        set() {}
+      }
+      const setStub = sinon.stub(IoRedis.prototype, 'set').callsFake(() => {});
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      const client = new Redis(options);
+      client.set('foo', 'bar', 0);
+      expect(setStub.calledWithExactly('foo', JSON.stringify('bar'))).to.be.true;
+    });
+
+    it('set method should be called with EX parameter when ttl is a positive number', () => {
+      class IoRedis {
+        set() {}
+      }
+      const setStub = sinon.stub(IoRedis.prototype, 'set').callsFake(() => {});
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      const client = new Redis(options);
+      client.set('foo', 'bar', 1);
+      expect(setStub.calledWithExactly('foo', JSON.stringify('bar'), 'EX', 1)).to.be.true;
+    });
+
+    it('set method should be called with EX parameter when ttl is a negative number', () => {
+      class IoRedis {
+        set() {}
+      }
+      const setStub = sinon.stub(IoRedis.prototype, 'set').callsFake(() => {});
+      const Redis = proxyquire('../../lib/providers/redis', { ioredis: IoRedis });
+      const client = new Redis(options);
+      client.set('foo', 'bar', -1);
+      expect(setStub.calledWithExactly('foo', JSON.stringify('bar'), 'EX', -1)).to.be.true;
+    });
+  });
+});
